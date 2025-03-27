a3a9f51b27b2730801183d57dad4688f77f2a5a8
evefonwu <evefonwu@gmail.com>
2025-03-27 19:12:34 -0400

    add description for completed tasks

--- app/page.tsx
+++ app/page.tsx
@@ -5,21 +5,27 @@ const links = [
   {
     id: 1,
     name: "Testimonial Card",
+    description: "User testimonial card",
     href: "./testimonial",
   },
   {
     id: 2,
     name: "Blog Card",
+    description: "Article/blog card with cover image",
     href: "./blog",
   },
   {
     id: 3,
     name: "Profile Card",
+    description:
+      "Profile card with contact button and social links for normal, hover, and focus states",
     href: "./profile",
   },
   {
     id: 4,
     name: "Hero Section",
+    description:
+      "Responsive hero section for desktop, tablet, and mobile views",
     href: "./hero",
   },
 ];
@@ -27,10 +33,15 @@ export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <ul>
+        <ul className={styles.container}>
           {links.map((item) => (
-            <li key={item.id}>
-              <Link href={item.href}>{item.name}</Link>
+            <li key={item.id} className={styles.item}>
+              <>
+                <p>
+                  <Link href={item.href}>{item.name}</Link>
+                </p>
+                <p>{item.description}</p>
+              </>
             </li>
           ))}
         </ul>

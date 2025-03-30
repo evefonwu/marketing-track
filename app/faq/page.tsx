248f6856f4256efc0cbb727cac671a709bce4f05
evefonwu <evefonwu@gmail.com>
2025-03-30 12:26:36 -0400

    fix faq description

--- app/faq/page.tsx
+++ app/faq/page.tsx
@@ -72,7 +72,8 @@ export default function Page() {
           <div className={styles.faq}>
             <div className={styles.faqHeader}>
               <h1>Frequently asked questions</h1>
-              <p>Get all your questions answered</p>
+              <p>Choose any questions you need</p>
+              {/* Mobile: <p>Get all your questions answered</p> */}
             </div>
             <div className={styles.faqContent}>
               <ul>
